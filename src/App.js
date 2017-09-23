a1f9c9b3a9ac65e91b75007e95613da4f7226c04
Scottie Schneider <scott@laclave.co>
2017-09-23 08:36:33 -0500

    Added in state for the first time, with Score

--- src/App.js
+++ src/App.js
@@ -29,11 +29,15 @@ Header.propTypes = {
 }
 
 class Counter extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {score: 0};
+  }
   render(){
     return(
       <div className="counter">
         <button className="counter-action decrement">-</button>
-        <div className="counter-score">{this.props.score}</div>
+        <div className="counter-score">{this.state.score}</div>
         <button className="counter-action increment">+</button>
       </div>
     )
