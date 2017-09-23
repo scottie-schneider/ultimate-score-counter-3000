4aaa2453a7cb711696f37d57261d874169b66bb8
Scottie Schneider <scott@laclave.co>
2017-09-23 08:30:04 -0500

    Good morning! ✅ Refactored Counter to class

--- src/App.js
+++ src/App.js
@@ -28,15 +28,16 @@ Header.propTypes = {
   title: PropTypes.string,
 }
 
-function Counter(props){
-  return(
-
+class Counter extends React.Component{
+  render(){
+    return(
       <div className="counter">
         <button className="counter-action decrement">-</button>
-        <div className="counter-score">{props.score}</div>
+        <div className="counter-score">{this.props.score}</div>
         <button className="counter-action increment">+</button>
       </div>
-  )
+    )
+  }
 }
 
 Counter.propTypes = {
